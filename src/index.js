68ebb9f09367abb56f920887a44e96ec89c7cc47
Váradi Zoltán <zoltan.varadi@ponte.hu>
2020-05-17 19:37:03 +0200

    Update dependencies and add Respresso live localization example

--- src/index.js
+++ src/index.js
@@ -1,28 +1,67 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-import { IntlProvider, addLocaleData } from 'react-intl';
-import messages_en from './respresso/localization/respresso.strings-en';
+import {addLocaleData, IntlProvider} from 'react-intl';
 import locale_en from 'react-intl/locale-data/en';
 import locale_de from 'react-intl/locale-data/de';
 import flatten from 'flat';
+import _ from 'lodash'
+import {RespressoLiveSync} from 'respresso-live-sync';
+
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import messages_en from './respresso/localization/respresso.strings-en';
 
 addLocaleData([...locale_en, ...locale_de]);
 
-const messages = {
-  en: messages_en
-};
-let language = navigator.language.split(/[-_]/)[0];
-language = messages.hasOwnProperty(language) ? language : 'en';
+const language = 'en';
+const enableRespressoLiveSync = false;
+
+let respressoLiveSyncClient = null;
+if (enableRespressoLiveSync) {
+	respressoLiveSyncClient  = new RespressoLiveSync({
+		projectToken: 'ec9a1576-7fc9-416e-9f7b-414ea2123cde', //keep in sync with respresso.json
+		version: '1.0.0', //keep in sync with respresso.json
+		appName: 'My React App: ' + navigator.vendor + ' (' + navigator.platform + ')'
+	});
+
+	respressoLiveSyncClient.enableDocumentSelectHandler();
+}
+
+class Top extends Component {
+
+	constructor(props, context) {
+		super(props, context);
+
+		let msg = {
+			en: flatten(messages_en)
+		};
+
+		this.state = {messages: _.cloneDeep(msg)};
+
+		if (enableRespressoLiveSync) {
+			respressoLiveSyncClient.onValueChangedListener = (values) => {
+				Object.assign(msg.en, values);
+
+				this.setState({
+					messages: _.cloneDeep(msg)
+				});
+			}
+		}
+	}
+
+	render() {
+		return (
+			<IntlProvider locale={language} messages={this.state.messages[language]}>
+				<App/>
+			</IntlProvider>
+		);
+	}
+}
 
 ReactDOM.render(
-  <IntlProvider locale={language} messages={flatten(messages[language])}>
-    <App />
-  </IntlProvider>,
-  document.getElementById('root')
+	<Top/>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
